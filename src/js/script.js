dd9561e280f8948a20f5220284d9996752387203
amiratax <faraji.ata92@gmail.com>
2022-08-01 16:12:32 +0430

    [ADD] section 3 and swiping

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,15 @@
-document.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowLeft") {
-    console.log("Left key is pressed.");
+const root = document.documentElement;
+const slide_num = document.querySelectorAll("section");
+
+let index = 0;
+
+root.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowLeft" && index > 0) {
+    index -= 1;
+    root.style.setProperty("--percentage", index * -100 + "%");
   }
-  if (e.key === "ArrowRight") {
-    console.log("Right key is pressed.");
+  if (e.key === "ArrowRight" && index < slide_num.length - 1) {
+    index += 1;
+    root.style.setProperty("--percentage", index * -100 + "%");
   }
 });
