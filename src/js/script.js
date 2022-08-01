8a15d44a376ebb26cccdf0acc047a8efe909a552
amiratax <faraji.ata92@gmail.com>
2022-08-01 12:49:18 +0430

    [ADD] section 1 and 2

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("keydown", (e) => {
+  if (e.key === "ArrowLeft") {
+    console.log("Left key is pressed.");
+  }
+  if (e.key === "ArrowRight") {
+    console.log("Right key is pressed.");
+  }
+});
