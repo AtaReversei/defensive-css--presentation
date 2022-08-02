b772c6eb0aab5c40bb2b08de35900845f754c784
amiratax <faraji.ata92@gmail.com>
2022-08-02 10:51:05 +0430

    [ADD] section 9, 10 and 11

--- src/js/script.js
+++ src/js/script.js
@@ -4,11 +4,15 @@ const slide_num = document.querySelectorAll("section");
 let index = 0;
 
 root.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowLeft" && index > 0) {
+  if (e.key === "ArrowLeft" && index > 0 && window.pageYOffset === 0) {
     index -= 1;
     root.style.setProperty("--percentage", index * -100 + "%");
   }
-  if (e.key === "ArrowRight" && index < slide_num.length - 1) {
+  if (
+    e.key === "ArrowRight" &&
+    index < slide_num.length - 1 &&
+    window.pageYOffset === 0
+  ) {
     index += 1;
     root.style.setProperty("--percentage", index * -100 + "%");
   }
